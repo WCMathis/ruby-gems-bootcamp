b60602791aa7c3833c33db8c19d2594a85f6ef6e
WCMathis <williammathis64@gmail.com>
2021-08-15 22:36:22 +0100

    Course scaffold added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  root 'static_pages#landing_page'
+  resources :courses
+  root 'home#index'
   #get 'static_pages/privacy_policy'
-  get "privacy_policy", to: "static_pages#privacy_policy"
+  #get "privacy_policy", to: "static_pages#privacy_policy"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
